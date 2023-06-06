7add8dffa550f35d3000040deb81415e565d2f23
D.T <dancingonmoon@msn.com>
2023-06-06 17:11:21 +0800

    FinRobot V2

--- FinanceRobot_Backtest.py
+++ FinanceRobot_Backtest.py
@@ -29,9 +29,9 @@ from BTCCrawl_To_DataFrame_Class import get_api_key
 
 if __name__ == '__main__':
     # 调用BTC爬取部分
-    sys.path.append("l:/Python_WorkSpace/量化交易/")  # 增加指定的绝对路径,进入系统路径,从而便于该目录下的库调用
-    Folder_base = "l:/Python_WorkSpace/量化交易/data/"
-    config_file_path = "l:/Python_WorkSpace/量化交易/BTCCrawl_To_DataFrame_Class_config.ini"
+    sys.path.append("e:/Python_WorkSpace/量化交易/")  # 增加指定的绝对路径,进入系统路径,从而便于该目录下的库调用
+    Folder_base = "e:/Python_WorkSpace/量化交易/data/"
+    config_file_path = "e:/Python_WorkSpace/量化交易/BTCCrawl_To_DataFrame_Class_config.ini"
     # URL = "https://api.coincap.io/v2/candles?exchange=binance&interval=h12&baseId=bitcoin&quoteId=tether"
     URL = 'https://data.binance.com'
     StartDate = "2023-1-20"
@@ -44,12 +44,12 @@ if __name__ == '__main__':
     BTC_data = BTC_DataAcquire(URL, StartDate, EndDate, Folder_base, BTC_json,
                                binance_api_key=api_key, binance_api_secret=api_secret)
     data = BTC_data.MarketFactor_ClosePriceFeatures(by_BinanceAPI=True,
-                                                    FromWeb=False, close_colName='close', lags=0, window=20, horizon=5,
+                                                    FromWeb=False, close_colName='close', lags=0, window=20, horizon=10,
                                                     interval='12h', MarketFactor=True, weekdays=7)
 
     batch_size = 32
     data_normalized = data_normalization(data, 365, )
-    dataset = Dataset_Generator(data_normalized)
+    dataset = Dataset_Generator(data_normalized, lags=7, shuffle=False)
     env = Finance_Environment_V2(dataset, action_n=3, min_performance=0., min_accuracy=0.1)  # 允许做空,允许大亏,使得更多的训练数据出现
     init_state, init_non_state = env.reset()
     action = env.action_space.sample()
@@ -60,7 +60,8 @@ if __name__ == '__main__':
     Q = Decompose_FF_Linear(seq_len=lags, in_features=obs_n, out_features=action_n, )
     Q_target = deepcopy(Q)
     # actions = model(state) # (N,1, action_n)
-    FinR_Agent = FinRobotAgentDQN(Q, Q_target, gamma=0.90, learning_rate=5e-4, learn_env=env, fit_batch_size=64, )
+    FinR_Agent = FinRobotAgentDQN(Q, Q_target, gamma=0.50, learning_rate=5e-4, learn_env=env, memory_size=2000,
+                                  replay_batch_size=1000, fit_batch_size=32, )
     # 训练过程:
     # FinR_Agent.learn(episodes=80)
     # print(f"{'-' * 40}finished{'-' * 40}")
@@ -135,7 +136,6 @@ if __name__ == '__main__':
         # hovertemplate='日期:%{x},价格: %{y:$.0f}',
     )
 
-
     layout = dict(
         title=dict(text='事件型回测:', font=dict(
             color='rgb(0,125,125)', family='SimHei', size=20)),
@@ -160,6 +160,6 @@ if __name__ == '__main__':
     # 获取回测数据最后一个交易日,先获得UTC时区,再转化成shanghai时区;再格式化成日期时刻
     last_date = BacktestEvent.net_wealths.index[-1].tz_localize('UTC').tz_convert('Asia/Shanghai')
     last_date = last_date.strftime('%y%m%dH%H')
-    saved_path = '{}_{}.html'.format(saved_path_prefix,last_date)
+    saved_path = '{}_{}.html'.format(saved_path_prefix, last_date)
 
     fig.write_html(saved_path, include_plotlyjs=True, auto_open=False)
